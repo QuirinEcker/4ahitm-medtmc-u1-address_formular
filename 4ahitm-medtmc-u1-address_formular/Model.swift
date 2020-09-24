f787c790f1c548a2d3ead4f497fdb1eb904bdbac
Quirin <quirin.ecker@bajupa.com>
2020-09-24 22:18:55 +0200

    finished exercise error needs to be fixed

---
+++ 4ahitm-medtmc-u1-address_formular/Model.swift
@@ -0,0 +1,14 @@
+//
+//  Model.swift
+//  4ahitm-medtmc-u1-address_formular
+//
+//  Created by Quirin Ecker on 24.09.20.
+//  Copyright © 2020 Quirin Ecker. All rights reserved.
+//
+
+class Model {
+    var firstName = ""
+    var lastName = ""
+    var street = ""
+    var place = ""
+}
--- 4ahitm-medtmc-u1-address_formular/ViewController.swift
+++ 4ahitm-medtmc-u1-address_formular/ViewController.swift
@@ -10,11 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var model = Model()
+    @IBOutlet weak var lastNameField: UITextField!
+    @IBOutlet weak var firstNameField: UITextField!
+    @IBOutlet weak var streetNameField: UITextField!
+    @IBOutlet weak var placeField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onClick(_ sender: Any) {
+        model.firstName = firstNameField.text!
+        model.lastName = lastNameField.text!
+        model.street = streetNameField.text!
+        model.place = placeField.text!
+        
+        print("First Name: \(model.firstName)")
+        print("Last Name: \(model.lastName)")
+        print("Street: \(model.street)")
+        print("Place: \(model.place)")
+    }
+    
 }
 
